22353c1b43cdac00853aaaf5a6b3b84e724e5497
Quang VInh Dang <qdang@atimi.com>
2024-01-02 16:16:27 -0800

    Only take grayscale image, resize to 80x80 and stack 4 frame in 1 obs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import gymnasium as gym
+from gymnasium.wrappers import FrameStack, GrayScaleObservation, ResizeObservation
+from PIL import Image
+import numpy as np
 
 if __name__ == "__main__":
 
@@ -6,14 +9,18 @@ if __name__ == "__main__":
     max_step = 600
 
     env = gym.make("ALE/Pong-v5", render_mode = "human")
+    env = GrayScaleObservation(env)
+    env = ResizeObservation(env, (80, 80))
+    env =FrameStack(env, 4)
     obs, info = env.reset()
+    
 
     for episode in range(n_training_episode):
         for step in range(max_step):
             action = env.action_space.sample()
 
             obs, reward, terminate, done, info = env.step(action)
-            
+                        
             if terminate or done:
                 obs, info = env.reset()
                 break

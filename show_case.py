60e4eef235182ddc1d8d9777bee79793da922350
Quang Vinh Dang <dqvinh808@gmail.com>
2024-01-03 20:20:42 -0800

    Update model channel

--- show_case.py
+++ show_case.py
@@ -6,17 +6,13 @@ from model import Model
 import matplotlib.pyplot as plt
 import torch
 
-def plot_reward(reward_list, episode_list):
-    plt.plot(episode_list, reward_list)
-    plt.show()
-
 if __name__ == "__main__":
 
-    n_training_episode = 100_000
+    n_training_episode = 10_000
     max_step = 1
 
     ## Environment and things related
-    env = gym.make("ALE/Pong-v5", render_mode = "rgb_array")
+    env = gym.make("ALE/Pong-v5", render_mode = "human")
     env = GrayScaleObservation(env)
     env = ResizeObservation(env, (80, 80))
     env =FrameStack(env, 4)
@@ -29,6 +25,7 @@ if __name__ == "__main__":
         output_size=env.action_space.n
     )
     model.to(model.device) #Why on earth we need to do this?
+    model.load_state_dict(torch.load("model_pong.pt"))
 
     #Parameters
     gamma = 0.99
@@ -37,7 +34,8 @@ if __name__ == "__main__":
     max_expsilon = 1
     epsilon_decay = 0.005
 
-    MIN_MEMORY = 10_000
+    # MIN_MEMORY = 10_000
+    MIN_MEMORY = 1000
     
     #Plot
     reward_list = []
@@ -49,38 +47,13 @@ if __name__ == "__main__":
 
     for episode in range(n_training_episode):
         
-        epsilon = min_epsilon + (max_expsilon - min_epsilon) * np.exp(-epsilon_decay * episode)
+        epsilon = 0
         
         for step in range(max_step):
 
             action = model.choose_action(obs, epsilon)
             new_obs, reward, terminate, done, info = env.step(action)
             reward_per_run += reward
-
-            item = Buffer(
-                state=obs, 
-                next_state=new_obs,
-                action=action,
-                reward = reward,
-                done=done
-            )
-
-            relay_buffer.push(item)
-
-            if len(relay_buffer) > MIN_MEMORY:
-                if episode % 500 == 0:
-                    buffer = relay_buffer.sample(MIN_MEMORY)
-                    state_batch, next_state_batch, reward_batch, action_batch, done_batch = model.process_buffer(buffer)
-                    # import ipdb; ipdb.set_trace()
-                    q_value = model(state_batch).to(model.device)
-                    q_value_next_state = model(next_state_batch)
-                    expected_q_value = reward + gamma * q_value_next_state * (1 - done)
-
-                    loss = (q_value - expected_q_value).pow(2).mean()
-                    # Optimize the model
-                    model.optimizer.zero_grad()
-                    loss.backward()
-                    model.optimizer.step()
 
             ## Reset obs
             obs = new_obs
@@ -94,7 +67,7 @@ if __name__ == "__main__":
         episode_list.append(episode)
         epsilons_list.append(epsilon)
 
-        if episode % 500 == 0:
+        if episode % 1 == 0:
 
             axs[0].clear()
             axs[0].plot(episode_list, reward_list)
@@ -109,8 +82,6 @@ if __name__ == "__main__":
                 display.clear_output(wait=True)
                 display.display(fig)
 
-            model.save_model(f"model_step_{episode}.pt")
 
     plt.tight_layout()
     plt.show()
-    model.save_model("model_pong.pt")
